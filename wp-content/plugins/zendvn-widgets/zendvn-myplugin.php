53b39ca69407696f50d84c025ecf8774d02f095c
truong04052003 <tranvantruong10b2@gmail.com>
2023-04-18 08:58:35 +0700

    wedget-api

--- wp-content/plugins/zendvn-widgets/zendvn-myplugin.php
+++ wp-content/plugins/zendvn-widgets/zendvn-myplugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Plugin Name: ZendVN MyPlugin
+ Plugin Name: ZendVN WidgetAPI
 Plugin URI: http://www.zend.vn
 Description: Tim hieu ve qua trinh chuan xay dung Plugin 10.
 Author: ZendVN group
